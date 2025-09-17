16d8b2c2f47ba129d73d42b20a10da9cd5929a55
Dan Sarlo <81384153+dsarlo-viso@users.noreply.github.com>
2025-09-17 12:46:56 -0400

    Removed iqr tools and updated to spring ai 1.1.0-M1 (#12)

--- src/main/java/com/visotrust/viso/visomcpserver/model/iqr/IqrTrustRequest.java
+++
@@ -1,6 +0,0 @@
-/* Copyright (c) 2025 VISO TRUST */
-package com.visotrust.viso.visomcpserver.model.iqr;
-
-import jakarta.validation.constraints.NotNull;
-
-public record IqrTrustRequest(@NotNull String question) {}
